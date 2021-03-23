7b0991d51da1d58a155d6925b27766e93baac2da
Anvesh8263 <80801707+Anvesh8263@users.noreply.github.com>
2021-03-23 21:12:12 +0530

    Add files via upload

---
+++ armstrong.py
@@ -0,0 +1,20 @@
+num=int(input("Enter the number"))
+
+sum=0
+
+pow= len(str(num))
+
+temp=num
+
+while(temp>0):
+     
+      rem= temp%10
+      sum= sum+rem**pow
+      temp= temp//10
+if num==sum:
+   
+    print(num,"Number is Armstrong")
+
+else:
+    
+    print(num,"Number is not a Armstrong")
