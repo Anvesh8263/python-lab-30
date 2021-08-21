1343d7c4211be653ccb99737c05b14621d10d18c
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-08-21 08:30:14 +0530

    Add files via upload

---
+++ Bracket_Balance.py
@@ -0,0 +1,29 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def pairs_stack(string):
+
+    opening = list('[{(')
+
+    closing = list(']})')
+
+    match = list()
+
+    for s in string:
+        if s in opening:
+            match.insert(0, s)
+        elif s in closing:
+            if len(match) == 0:
+                return False
+            if match[0] == opening[closing.index(s)]:
+                match.pop(0)
+            else:
+                return False
+
+    if len(match) == 0:
+        return True
+
+    return False
+
+
+
+print(pairs_stack('((2[4]))'))
