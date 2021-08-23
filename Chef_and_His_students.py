79708b3f94cf55c9f9c6a879f0d502726d4a11c7
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-08-23 10:42:31 +0530

    Delete Chef_and_His_students.py

--- Chef_and_His_students.py
+++
@@ -1,9 +0,0 @@
-Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> for i in range(int(input())):
-    s=input()
-    a=s.count('<>')
-    if a>0:
-        print(a)
-    else:
-        print(0)
