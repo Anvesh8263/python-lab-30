04242b40d21750721c621c281bbf40a0321e5d03
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-08-23 10:41:54 +0530

    Delete burj_khalifa_binary.py

--- burj_khalifa_binary.py
+++
@@ -1,6 +0,0 @@
-Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> n = int(input())
-l = len(str(bin(n))[2:])
-for i in range(n+1):
-    print(str(bin(i))[2:].rjust(l))
