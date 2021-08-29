19d3bd7cef3b08fc9b6635aee7c372a028bcca69
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-08-29 08:44:25 +0530

    Add files via upload

---
+++ exception_example.py
@@ -0,0 +1,8 @@
+ a = int(input('enter the number of line '))
+
+for i in range(a):
+    try:
+        n, r = map(int,input().split())
+        print(n/r)
+    except Exception as e:
+        print(e)
