6df64be36bd05dc853237b41c4dbcd3a1f155abe
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-09-04 13:15:16 +0530

    Add files via upload

---
+++ The_Bar.py
@@ -0,0 +1,57 @@
+ li=[]
+
+while True:
+
+    try:line=input()
+
+    except EOFError :break
+
+    li.append(line.strip())
+
+    
+
+li=''.join(li)
+
+d=eval(li)
+
+s=set()
+
+l=list(d.values())
+
+for i in d:
+
+    for j in d[i]:
+
+        s.add(j)
+
+l2=sorted(list(s))
+
+c=0
+
+while len(l)!=0:
+
+    l3=[]
+
+    for i in l:
+
+        for j in i:
+
+            l3.append(j)
+
+    d2={i:l3.count(i) for i in l2}
+
+    m=max(d2,key=d2.get)
+
+    l4=[]
+
+    for i in l:
+
+        if m not in i:
+
+            l4.append(i)
+
+    l=l4
+
+    c+=1
+
+print(c)
