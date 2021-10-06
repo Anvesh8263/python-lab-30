2e200c28edbf18dfc194f63837627c1e9653f357
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-10-06 21:30:11 +0530

    Add files via upload

---
+++ Merge_the_tools.py
@@ -0,0 +1,11 @@
+def merge_the_tools(string, k):
+    c=0
+    s=''
+    for i in string:
+        if i not in s:
+            s=s+i
+        c+=1
+        if(c==k):
+            print(s)
+            c=0
+            s=''
